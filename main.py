5ae5818c18683fae0f51860bcedfd28f3017a3be
Travis Eno <Travis.Eno@live.com>
2022-09-12 20:24:14 -0500

    Initial commit of application code
    First commit of code for the FastAPI python webapp.
    
    Includes a "method" module that contains many of the functions used in the main.py file.
    
    styles.css is a generic style sheet for the output table.
    
    table.html is a Jinja2 template that displays the card data as a dynamic HTML table

---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+import hvac
+from time import perf_counter
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+from method import card_methods
+
+load_dotenv()
+
+
+# .env file boilerplate code to read secret from .env file in local directory
+### Not needed if using Vault server ###
+# MY_CLIENT_ID = os.getenv("MY_CLIENT_ID")
+# MY_CLIENT_SECRET = os.getenv("MY_CLIENT_SECRET")
+
+
+# # Set Vault variables to be used in init_server() and read_secret()
+# ### Not needed if using .env file to store secrets locally ###
+MY_VAULT_URL = "http://localhost:8200"
+VAULT_PATH = "hs_api_creds"
+
+# Hashicorp Vault boilerplate code to read secret from Vault "Dev" server
+def init_server():
+
+    client = hvac.Client(url=f"{MY_VAULT_URL}")
+    print(f" Is client authenticated: {client.is_authenticated()}")
+    return client
+
+
+def read_secret(client):
+
+    read_response = client.secrets.kv.v2.read_secret_version(path=f"{VAULT_PATH}")
+    print(read_response)
+    return read_response
+
+
+# Initiate server and read secret from Vault server
+client_data = init_server()
+read_data = read_secret(client_data)
+
+MY_CLIENT_ID = read_data.get("data").get("data").get("MY_CLIENT_ID")
+MY_CLIENT_SECRET = read_data.get("data").get("data").get("MY_CLIENT_SECRET")
+
+
+# Start of FastAPI code to create web app
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_cards_for_druid_warlock(request: Request):
+
+    t1_start = perf_counter()
+
+    # Get cards for Druid and Warlock
+    # By default, the get_cards() method will return cards for Druid and Warlock
+    # By default, the get_cards() method will return 10 cards
+    data = card_methods.get_cards(MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    (
+        set_data,
+        class_data,
+        type_data,
+        rarity_data,
+    ) = card_methods.get_all_metadata_variables(MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    # Replace card metadata with human readable names
+    card_methods.replace_card_metadata(
+        data, set_data, rarity_data, class_data, type_data
+    )
+
+    headings = card_methods.create_formatted_headers(data)
+
+    data_list = card_methods.format_table_data_for_jinja(data)
+
+    t1_stop = perf_counter()
+
+    print("Elapsed time during the whole program in seconds:", t1_stop - t1_start)
+
+    return templates.TemplateResponse(
+        "table.html",
+        {
+            "request": request,
+            "headings": headings,
+            "data": data_list,
+        },
+    )
+
+
+@app.get("/class/{class_name}", response_class=HTMLResponse)
+async def get_cards_for_class(request: Request, class_name: str):
+
+    t1_start = perf_counter()
+
+    # Get all cards for the class name passed in the url path parameter
+    # By default the get_cards_from_class() method will return 10 cards
+    data = card_methods.get_cards_from_class(class_name, MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    (
+        set_data,
+        class_data,
+        type_data,
+        rarity_data,
+    ) = card_methods.get_all_metadata_variables(MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    # Replace card metadata with human readable names
+    card_methods.replace_card_metadata(
+        data, set_data, rarity_data, class_data, type_data
+    )
+
+    headings = card_methods.create_formatted_headers(data)
+
+    data_list = card_methods.format_table_data_for_jinja(data)
+
+    t1_stop = perf_counter()
+
+    print("Elapsed time during the whole program in seconds:", t1_stop - t1_start)
+
+    return templates.TemplateResponse(
+        "table.html",
+        {
+            "request": request,
+            "headings": headings,
+            "data": data_list,
+        },
+    )
---
+++ method/card_methods.py
@@ -0,0 +1,188 @@
+import requests
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+import hvac
+
+
+def get_token(client_id, client_secret):
+    client = BackendApplicationClient(client_id=client_id)
+    oauth = OAuth2Session(client=client)
+
+    token = oauth.fetch_token(
+        token_url="https://us.battle.net/oauth/token",
+        client_id=client_id,
+        client_secret=client_secret,
+    )
+    return token.get("access_token")
+
+
+# def requests_session_token():
+#     s = requests.Session()
+#     s.headers.update({"Content-Type": "application/json"})
+#     s.headers.update({"access_token": response.headers.get("CSRFToken")})
+
+
+def hs_api_call(access_token):
+    request_url = f"https://us.api.blizzard.com/hearthstone/cards?locale=en_US&manaCost=7%2C8%2C9%2C10&class=warlock,druid&rarity=legendary&access_token={access_token}"
+    return requests.get(request_url, timeout=5)
+
+
+def hs_api_call_single(access_token, class_1):
+    request_url = f"https://us.api.blizzard.com/hearthstone/cards?locale=en_US&manaCost=7%2C8%2C9%2C10&class={class_1}&rarity=legendary&access_token={access_token}"
+    return requests.get(request_url, timeout=5)
+
+
+def get_cards(MY_CLIENT_ID, MY_CLIENT_SECRET):
+
+    my_access_token = get_token(MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    result = (hs_api_call(my_access_token)).json()
+
+    # jsonResponse = result.json()
+
+    card_list_response = result.get("cards")
+
+    # print(listresponse[0])
+    # print(listresponse[0]["id"])
+    # print(listresponse[0]["name"])
+
+    new_list = []
+    for x in card_list_response:
+        new_dict = {
+            "id": x["id"],
+            "image": x["image"],
+            "name": x["name"],
+            "cardTypeId": x["cardTypeId"],
+            "rarityId": x["rarityId"],
+            "cardSetId": x["cardSetId"],
+            "classId": x["classId"],
+        }
+        new_list.append(new_dict)
+
+    # Sorting list by "id" value from each dict object in the list
+    newlist = sorted(new_list, key=lambda d: d["id"], reverse=True)
+
+    # newlist[0].pop("id")
+    # Return the first 10 items in the list
+    return newlist[:11]
+
+
+def get_cards_from_class(class_1, MY_CLIENT_ID, MY_CLIENT_SECRET):
+
+    my_access_token = get_token(MY_CLIENT_ID, MY_CLIENT_SECRET)
+
+    result = hs_api_call_single(my_access_token, class_1)
+    # result = hs_api_call(my_access_token)
+
+    jsonResponse = result.json()
+
+    listresponse = jsonResponse.get("cards")
+
+    new_list = []
+    for x in listresponse:
+        new_dict = {
+            "id": x["id"],
+            "image": x["image"],
+            "name": x["name"],
+            "cardTypeId": x["cardTypeId"],
+            "rarityId": x["rarityId"],
+            "cardSetId": x["cardSetId"],
+            "classId": x["classId"],
+        }
+        new_list.append(new_dict)
+
+    # Sorting list by "id" value from each dict object in the list
+    newlist = sorted(new_list, key=lambda d: d["id"], reverse=True)
+
+    # newlist[0].pop("id")
+
+    # Return the first 10 items in the list
+    return newlist[:11]
+
+
+def get_all_metadata(MY_CLIENT_ID, MY_CLIENT_SECRET):
+    """Get all metadata from Blizzard Hearthstone API and return as a list of dictionaries"""
+    my_access_token = get_token(MY_CLIENT_ID, MY_CLIENT_SECRET)
+    request_url = f"https://us.api.blizzard.com/hearthstone/metadata?locale=en_US&access_token={my_access_token}"
+    return (requests.get(request_url, timeout=5)).json()
+
+
+def replace_card_metadata(card_list, sets, rarities, classes, types):
+    for card in card_list:
+        for card_set in sets:
+            if card["cardSetId"] == card_set["id"]:
+                card["cardSetId"] = card_set["name"]
+        for rarity in rarities:
+            if card["rarityId"] == rarity["id"]:
+                card["rarityId"] = rarity["name"]
+        for class_ in classes:
+            if card["classId"] == class_["id"]:
+                card["classId"] = class_["name"]
+        for type_ in types:
+            if card["cardTypeId"] == type_["id"]:
+                card["cardTypeId"] = type_["name"]
+    return card_list
+
+
+def get_all_metadata_variables(MY_CLIENT_ID, MY_CLIENT_SECRET):
+    # Get all metadata for cards and store in variables for later use
+    all_meta_data = get_all_metadata(MY_CLIENT_ID, MY_CLIENT_SECRET)
+    set_data = all_meta_data.get("sets")
+    class_data = all_meta_data.get("classes")
+    type_data = all_meta_data.get("types")
+    rarity_data = all_meta_data.get("rarities")
+    return set_data, class_data, type_data, rarity_data
+
+
+def create_formatted_headers(card_data):
+    # Gather header names from first card
+    headers = list(card_data[0].keys())
+
+    # Remove cardId from headers
+    headers.pop(0)
+
+    # Replace headers with human readable names
+    for x in headers:
+        if x == "cardSetId":
+            headers[headers.index(x)] = "Set"
+        elif x == "cardTypeId":
+            headers[headers.index(x)] = "Type"
+        elif x == "classId":
+            headers[headers.index(x)] = "Class"
+        elif x == "rarityId":
+            headers[headers.index(x)] = "Rarity"
+        elif x == "name":
+            headers[headers.index(x)] = "Name"
+        elif x == "image":
+            headers[headers.index(x)] = "Card Image"
+
+    return headers
+
+
+def format_table_data_for_jinja(data):
+    """Format data for jinja2 template"""
+    data_list = []
+    for x in data:
+        data_list.append(list(x.values()))
+
+    for x in data_list:
+        x[1] = f'<img src="{x[1]}" width="200" >'
+        # Remove cardId from data
+        x.pop(0)
+
+    return data_list
+
+
+# Hashicorp Vault boilerplate code to read secret from Vault "Dev" server
+def init_server():
+
+    client = hvac.Client(url=f"{MY_VAULT_URL}")
+    print(f" Is client authenticated: {client.is_authenticated()}")
+    return client
+
+
+def read_secret(client):
+
+    read_response = client.secrets.kv.v2.read_secret_version(path=f"{VAULT_PATH}")
+    print(read_response)
+    return read_response
